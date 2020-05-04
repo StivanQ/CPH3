4ec38fa139532a353501cda256345b56b6b831be
Anton-Fabian PATRAŞ (100896) <anton_fabian.patras@stud.acs.upb.ro>
2020-05-04 10:14:19 +0300

    [#3]

--- src/client.c
+++ src/client.c
@@ -36,38 +36,12 @@ int main(int argc, char *argv[])
 
     // Ex 1.2: POST dummy and print response from main server
 
-    sockfd = open_connection("188.166.16.132", 80, AF_INET, SOCK_STREAM, 0);
-
-    char* body_data[2];
-    body_data[0] = "w/e mesaj";
-
-    message = compute_post_request("api.openweathermap.org",
-                                     "/api/v1/dummy", 
-                                    "application/x-www-form-urlencoded", body_data, 
-                                    1, NULL, 0);
-    printf("{%s}\n", message);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    printf("{{{{%s}}}}\n", response);
-
     // Ex 2: Login into main server
     // char *compute_post_request(char *host, char *url, char* content_type, char **body_data,
     //                         int body_data_fields_count, char **cookies, int cookies_count)
-    body_data[0] = "username=student&password=student";
-    message = compute_post_request("api.openweathermap.org",
-                                     "/api/v1/auth/login", 
-                                    "application/x-www-form-urlencoded", body_data, 
-                                    1, NULL, 0);
-
-
-    printf("{%s}\n", message);
-    send_to_server(sockfd, message);
-    response = receive_from_server(sockfd);
-    printf("{{{{%s}}}}\n", response);
 
 
     // Ex 3: GET weather key from main server
-
 
     // Ex 4: GET weather data from OpenWeather API
     // Ex 5: POST weather data for verification to main server
